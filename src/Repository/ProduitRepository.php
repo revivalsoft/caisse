b66ca538724e465d0c2a537e3e2b53dec76b04be
shartmo <michel.maushart@gmail.com>
2025-09-04 15:19:29 +0200

    sav 3 du 4 9 25

--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * Zoomerplanning - Logiciel de caisse pour restaurants
+ * Copyright (C) 2025 RevivalSoft
+ *
+ * Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou
+ * le modifier selon les termes de la Licence Publique Générale GNU publiée
+ * par la Free Software Foundation Version 3.
+ *
+ * Ce programme est distribué dans l'espoir qu'il sera utile,
+ * mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de
+ * COMMERCIALISATION ou D’ADÉQUATION À UN BUT PARTICULIER. Voir la
+ * Licence Publique Générale GNU pour plus de détails.
+ *
+ * Vous devriez avoir reçu une copie de la Licence Publique Générale GNU
+ * avec ce programme ; si ce n'est pas le cas, voir
+ * <https://www.gnu.org/licenses/>.
+ */
+
 namespace App\Repository;
 
 use App\Entity\Produit;
@@ -15,29 +33,4 @@ class ProduitRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Produit::class);
     }
-
-//    /**
-//     * @return Produit[] Returns an array of Produit objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Produit
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
